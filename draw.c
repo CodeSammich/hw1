a2b2a567fe82d0da684f38f27f0cab198c68c8cb
Samuel Zhang <samuelzhang3@gmail.com>
2016-02-24 02:33:38 -0500

    octants 1 working

--- draw.c
+++ draw.c
@@ -15,32 +15,18 @@ void draw_line(int x0, int y0, int x1, int y1, screen s, color c) {
     for(; x0 < x1; x0++ )
       plot( s, c, x0, y0 );
   }
-  else //everything else
-    if( x0 > 0 ) {
-      if( y0 >= 0 ) {
-	if( y0 <= x0 )
-	  first_octant( x0, y0, x1, y1, s, c );
-	else
-	  second_octant( x0, y0, x1, y1, s, c );
-      }
-      else if( y0 < 0 ) {
-	if( y0 <= -1 * x0 )
-	  seventh_octant( x0, y0, x1, y1, s, c );
-	else
-	  eigth_octant( x0, y0, x1, y1, s, c );
-      }
-    } else {
-      if( y0 >= 0 )
-	if( y0 <= -1 * x0 )
-	  third_octant( x0, y0, x1, y1, s, c );
-	else
-	  fourth_octant( x0, y0, x1, y1, s, c );
-      else
-	if( -1 * y0 <= -1 * x0 )
-	  fifth_octant( x0, y0, x1, y1, s, c );
-	else
-	  sixth_octant( x0, y0, x1, y1, s, c ); 
-    }
+  else {
+    //    printf("in\n");
+    double m = (y1 - y0) / (x1 - x0);
+    if( (m >= 0 && m <= 1) ) 
+      first_octant( x0, y0, x1, y1, s, c );
+    else if( (m > 1) )
+      second_octant( x0, y0, x1, y1, s, c );
+    else if( (m < -1 ))
+      third_octant( x0, y0, x1, y1, s, c );
+    else
+      fourth_octant( x0, y0, x1, y1, s, c );
+  }
 }
 
 void first_octant(int x0, int y0, int x1, int y1, screen s, color c) {
@@ -49,16 +35,16 @@ void first_octant(int x0, int y0, int x1, int y1, screen s, color c) {
 
   int dx = x1 - x0;
   int dy = y1 - y0;
-
-  int d = 2 * dx - dy;
+  
+  int d = 2 * dy - dx;
   while( x <= x1 ) {
     plot( s, c, x, y);
     if( d > 0 ) {
-      y += .5;
-      d -= 2 * dy;
+      y += 1;
+      d -= 2 * dx;
     }
     x += 1;
-    d += dx;
+    d += 2 * dy;
   }
 }
 
@@ -68,69 +54,24 @@ void second_octant(int x0, int y0, int x1, int y1, screen s, color c) {
 
   int dx = x1 - x0;
   int dy = y1 - y0;
-
-  int d = 2 * dy - dx;
-  while( y <= y1 ) {
+  
+  int d = 2 * dx - dy;
+  while( x <= x1 ) {
     plot( s, c, x, y);
     if( d < 0 ) {
       y += 1;
-      d += 2 * dx;
+      d -= 2 * dx;
     }
-    x += .5;
-    d -= dy;
+    x += 1;
+    d += dy;
   }
 }
 
 void third_octant(int x0, int y0, int x1, int y1, screen s, color c) {
-  int x = x0;
-  int y = y0;
 
-  int dx = x1 - x0;
-  int dy = y1 - y0;
-
-  int d = 2 * dy + dx;
-  while( y <= y1 ) {
-    plot( s, c, x, y);
-    if( d > 0 ) {
-      y -= 1;
-      d -= 2 * dx;
-    }
-    x += .5;
-    d -= dy;
-  }
 }
 
 void fourth_octant(int x0, int y0, int x1, int y1, screen s, color c) {
-  int x = x0;
-  int y = y0;
 
-  int dx = x1 - x0; 
-  int dy = y1 - y0;
-
-  int d = 2 * dx + dy;
-  while( x <= x1 ) {
-    plot( s, c, x, y);
-    if( d < 0 ) {
-      y -= .5;
-      d += 2 * dx;
-    }
-    x += 1;
-    d += dx;
-  }
 }
 
-void fifth_octant(int x0, int y0, int x1, int y1, screen s, color c) {
-  first_octant( x0, y0, x1, y1, s, c);
-}
-
-void sixth_octant(int x0, int y0, int x1, int y1, screen s, color c) {
-  second_octant( x0, y0, x1, y1, s, c);
-}
-
-void seventh_octant(int x0, int y0, int x1, int y1, screen s, color c) {
-  third_octant( x0, y0, x1, y1, s, c);
-}
-
-void eigth_octant(int x0, int y0, int x1, int y1, screen s, color c) {
-  fourth_octant( x0, y0, x1, y1, s, c);
-}
--- draw.h
+++ draw.h
@@ -7,9 +7,5 @@ void first_octant(int x0, int y0, int x1, int y1, screen s, color c);
 void second_octant(int x0, int y0, int x1, int y1, screen s, color c);
 void third_octant(int x0, int y0, int x1, int y1, screen s, color c);
 void fourth_octant(int x0, int y0, int x1, int y1, screen s, color c);
-void fifth_octant(int x0, int y0, int x1, int y1, screen s, color c);
-void sixth_octant(int x0, int y0, int x1, int y1, screen s, color c);
-void seventh_octant(int x0, int y0, int x1, int y1, screen s, color c);
-void eigth_octant(int x0, int y0, int x1, int y1, screen s, color c);
 
 #endif
